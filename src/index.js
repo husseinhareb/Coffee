90e6bf32e0af3eb05c8aa37e9149d91b954d4f3d
husseinhareb <shtam.hareb12@gmail.com>
2023-11-28 23:44:09 +0200

    index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        resizable: false,
         webPreferences: {
             nodeIntegration: true
         }
