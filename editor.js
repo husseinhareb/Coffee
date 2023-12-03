977aa8fe2cb1fceb24fec21d918750a6fa52df0a
husseinhareb <shtam.hareb12@gmail.com>
2023-12-03 21:45:38 +0200

    styling:style.css

--- editor.js
+++ editor.js
@@ -62,10 +62,3 @@ fs.readFile('keywords.json', 'utf8', (err, jsonData) => {
 });
 
 
-const fss = document.getElementById('fs');
-
-// Define content you want to write
-const content = 'This is the content I want to write into the div.';
-
-// Write content into the div using innerHTML
-fss.innerHTML = content;
---
+++ fileManager.js
@@ -0,0 +1,7 @@
+const fss = document.getElementById('fs');
+
+// Define content you want to write
+const content = 'This is the content I want to write into the div.';
+
+// Write content into the div using innerHTML
+fss.innerHTML = content;
