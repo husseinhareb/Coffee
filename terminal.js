6f31857767a22509615674a633f8d91102612384
husseinhareb <shtam.hareb12@gmail.com>
2024-01-11 20:58:29 +0200

    resizing terminal

--- terminal.js
+++ terminal.js
@@ -18,3 +18,56 @@ ipc.on("terminal.incomingData", (event, data) => {
 term.onData(e => {
     ipc.send("terminal.keystroke", e);
 });
+
+
+
+const terminal = document.getElementById('terminal');
+let isResizingHeight = false;
+let startY, startHeight;
+
+function handleMouseHeightMove(e) {
+  if (!isResizingHeight) return;
+
+  const newHeight = startHeight - (e.clientY - startY);
+  terminal.style.height = newHeight + 'px';
+  terminal.style.borderTop = "5px solid #29355a";
+}
+let hoverTimeout;
+
+terminal.addEventListener('mousemove', (e) => {
+  const isNearTopEdge = e.offsetY < 6;
+  clearTimeout(hoverTimeout);
+  hoverTimeout = setTimeout(() => {
+    terminal.style.transition = "border-top 0.5s ease"; // Transition property
+    terminal.style.borderTop = isNearTopEdge ? "5px solid #29355a" : "3px solid #1b1e2e";
+  }, 300);
+  terminal.style.cursor = isNearTopEdge ? "row-resize" : "default";
+});
+
+terminal.addEventListener('mouseleave', () => {
+  clearTimeout(hoverTimeout);
+
+  terminal.style.transition = "border-top 0.5s ease"; // Transition property
+  terminal.style.borderTop = "3px solid #1b1e2e";
+
+  setTimeout(() => {
+    terminal.style.transition = "none";
+  }, 300);
+});
+
+
+terminal.addEventListener('mousedown', (e) => {
+  const isNearTopEdge = e.offsetY < 6;
+  if (isNearTopEdge) {
+    isResizingHeight = true;
+    startY = e.clientY;
+    startHeight = terminal.offsetHeight; 
+    document.addEventListener('mousemove', handleMouseHeightMove);
+  }
+});
+
+document.addEventListener('mouseup', () => {
+  isResizingHeight = false;
+  document.removeEventListener('mousemove', handleMouseHeightMove);
+  terminal.style.borderTop = "3px solid #1b1e2e";
+});
