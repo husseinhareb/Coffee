9e68063ab076b0e6cb1aaeb1337d61ac12cd2d49
husseinhareb <shtam.hareb12@gmail.com>
2024-01-11 21:08:11 +0200

    styling

--- terminal.js
+++ terminal.js
@@ -38,7 +38,7 @@ terminal.addEventListener('mousemove', (e) => {
   const isNearTopEdge = e.offsetY < 6;
   clearTimeout(hoverTimeout);
   hoverTimeout = setTimeout(() => {
-    terminal.style.transition = "border-top 0.5s ease"; // Transition property
+    terminal.style.transition = "border-top 0.5s ease";
     terminal.style.borderTop = isNearTopEdge ? "5px solid #29355a" : "3px solid #1b1e2e";
   }, 300);
   terminal.style.cursor = isNearTopEdge ? "row-resize" : "default";
@@ -47,7 +47,7 @@ terminal.addEventListener('mousemove', (e) => {
 terminal.addEventListener('mouseleave', () => {
   clearTimeout(hoverTimeout);
 
-  terminal.style.transition = "border-top 0.5s ease"; // Transition property
+  terminal.style.transition = "border-top 0.5s ease"; 
   terminal.style.borderTop = "3px solid #1b1e2e";
 
   setTimeout(() => {
